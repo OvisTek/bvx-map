78389c22b700a9cc3d3da3699f4658267054a1db
David Arayan <david.arayan12@gmail.com>
2021-07-10 17:52:23 +1000

    - Fixed the VoxelMap.generate() function - Added optional parameter for density checking

--- src/map/voxel-map.ts
+++ src/map/voxel-map.ts
@@ -16,7 +16,7 @@ export class VoxelMap {
      * Generate a map for the provided Voxel Chunk
      * @param chunk - The Chunk to use for generating a Voxel
      */
-    public generate(chunk: VoxelChunk | null = null): void {
+    public generate(chunk: VoxelChunk | null = null, densityCheck: number = 0.8): void {
         if (!chunk) {
             throw new Error("VoxelMap.generate(chunk) - requires a valid chunk");
         }
@@ -24,32 +24,33 @@ export class VoxelMap {
         // this is the local coordinate of the provided chunk
         const localCoordinate: MortonKey = chunk.key;
         const vxSize: number = VoxelChunk.SIZE;
-        const btSize: number = VoxelChunk.BVX_SUBDIV;
+        const bvSize: number = VoxelChunk.BVX_SUBDIV;
+        const mapOffset: number = vxSize * bvSize;
 
         // convert from local to world coordinate
-        const worldCoordX: number = localCoordinate.x * vxSize;
-        const worldCoordY: number = localCoordinate.y * vxSize;
-        const worldCoordZ: number = localCoordinate.z * vxSize;
+        const worldCoordX: number = localCoordinate.x * mapOffset;
+        const worldCoordY: number = localCoordinate.y * mapOffset;
+        const worldCoordZ: number = localCoordinate.z * mapOffset
 
         const vindex: VoxelIndex = VoxelMap._tmpIndex;
         const gen: SimplexNoiseGenerator = this._generator;
 
-        const totalVoxels = (vxSize * vxSize * vxSize) * (btSize * btSize * btSize);
+        const totalVoxels = (vxSize * vxSize * vxSize) * (bvSize * bvSize * bvSize);
 
         // for every bit-voxel, we run the noise algorithm
         for (let vx: number = 0; vx < totalVoxels; vx++) {
             // sets the voxel index
             vindex.key = vx;
 
-            const worldCoordBVX: number = worldCoordX + vindex.vx + (vindex.bx / btSize);
-            const worldCoordBVY: number = worldCoordY + vindex.vy + (vindex.by / btSize);
-            const worldCoordBVZ: number = worldCoordZ + vindex.vz + (vindex.bz / btSize);
+            const worldCoordBVX: number = worldCoordX + (vindex.vx * vxSize) + vindex.bx;
+            const worldCoordBVY: number = worldCoordY + (vindex.vy * vxSize) + vindex.by;
+            const worldCoordBVZ: number = worldCoordZ + (vindex.vz * vxSize) + vindex.bz;
 
             // get the noise for the provided BitVoxel at provided world coordinate
             const density: number = gen.getDensity(worldCoordBVX, worldCoordBVY, worldCoordBVZ);
 
             // decide if we want to set or unset the bitvoxel at provided position
-            if (density > 0.8) {
+            if (density > densityCheck) {
                 chunk.setBitVoxel(vindex);
             }
             else {
